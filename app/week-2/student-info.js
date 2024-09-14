53acec70aa848fe958727509f45372dbed9f035c
Jiaping Liu <lemonmos@gmail.com>
2024-09-13 22:17:33 -0600

    add week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link"; 
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <p>Name: Jiaping Liu</p>
+        <Link href="https://github.com/Lemonmos/cprg306-assignments">Github Repository</Link>
+
+      </main>
+    );
+  }

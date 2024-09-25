24173348ff6e19a6e6e34cc8a04829b709b9add5
Jiaping Liu <lemonmos@gmail.com>
2024-09-25 01:01:30 -0600

    add week3

--- app/page.js
+++ app/page.js
@@ -2,12 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  let weeks = [2,3,4,5,6,7,8,9,10,11,12,13,14];
   return (
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <li>
-        <Link href="/week-2">Week-2</Link>
-      </li>
+      {weeks.map((week) => (
+        <li className="hover:text-green-500 text-pink-500 font-bold">
+        <Link href={`/week-${week}`}>Week-{week}</Link>
+        </li>
+    ))}
     </div>
   );
 }
---
+++ app/week-3/item.js
@@ -0,0 +1,10 @@
+export default function Item({name, quantity, category}) {
+    return (
+      <section className="bg-zinc-700 w-60">
+        <h3 className="text-white font-bold text-xl">{name},{quantity}</h3>
+        <p className="text-white">Buy {quantity} in {category}</p>
+      </section>
+    );
+
+
+}

c0f9d907e4e6f0b90dacbbfd28a45864b37f080b
Jiaping Liu <lemonmos@gmail.com>
2024-10-05 23:29:16 -0600

    modify week5

---
+++ app/week-5/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import {useState} from "react";
+import NewItem from "./new-item";
+
+export default function Page() {
+
+
+    return (
+        <main>
+            <NewItem />
+        </main>
+    );
+
+}
